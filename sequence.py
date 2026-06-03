cdb2a531cadcf104f7cea53191f0c9afad3a4f56
Shagoto sarkar <shagotosarkar@gmail.com>
2026-06-03 23:49:56 +0600

    added sequence.py

---
+++ sequence.py
@@ -0,0 +1,4 @@
+name = "shagoto"
+print(name[0]) #for the first element of the sequence
+print(name[-1]) #for the last element of the sequence
+for _ in name: print(_) # to print all the elements of the sequence
